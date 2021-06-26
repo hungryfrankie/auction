f66f3b77a291e47b246654e624eb9104e750b2db
hungryfrankie <85903276+hungryfrankie@users.noreply.github.com>
2021-06-26 21:02:16 +0430

    Update batch.php

--- batch.php
+++ batch.php
@@ -6,9 +6,7 @@
 
 /***************************************************************************
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version. Although none of the code may be
+ *   it under the terms of the GNU General Public L
  *   sold. If you have been sold this script, get a refund.
  ***************************************************************************/
 
